9dc209a7240e522b5b77c74fa5c6d99cd665df46
Helmi Yuliardi <115688584+miardi@users.noreply.github.com>
2024-12-16 11:05:57 +0700

    Update 2 Exercise.sql
    Latihan 2

--- 2 Exercise.sql
+++ 2 Exercise.sql
@@ -1,10 +1,24 @@
 1. Cari nama belakang "Doe":
+  SELECT * FROM Employees
+  WHERE Name LIKE '%Doe'
+  
 2. Cari JobTitle yang mengandung "Developer":
+  SELECT * FROM Employees
+  WHERE JobTitle LIKE '%Developer%'
 3. Cari email yang diakhiri "example.com":
+  SELECT * FROM Employees
+  WHERE email LIKE '%example.com'
 
 4. Temukan semua produk yang memiliki kata pertama "Laptop" di ProductName.
+  SELECT * FROM Products
+  WHERE ProductName LIKE 'Laptop'
+  
 5. Cari produk dengan nama yang dimulai dengan huruf "S".
+  SELECT * FROM Products
+  WHERE Name LIKE 'S%'
 6. Temukan produk dengan harga di atas 1000 dan nama mengandung "Pro".
+  SELECT* FROM Products
+  WHERE Harga > 1000 AND Name 
 
 7. Cari pelanggan yang nama depannya dimulai dengan huruf "A" menggunakan LEFT.
 8. Cari pelanggan yang memiliki titik (.) dalam alamat email menggunakan CHARINDEX.
