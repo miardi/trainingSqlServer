bbc4365666de3262a21aad2bdb8d96abf933c93a
miardi <helmiyuliardi@gmail.com>
2024-12-17 14:34:42 +0700

    Latihan 10

--- 10 Exercise.sql
+++ 10 Exercise.sql
@@ -3,6 +3,19 @@ Buat stored procedure bernama GetTotalSalaryByDepartment yang menghitung total g
 
 Jalankan stored procedure ini untuk Departemen IT (@DepartmentID = 3)
 
+CREATE PROCEDURE GetTotalSalaryByDepartmentMiardi 
+@DepartmentID INT
+AS
+BEGIN
+	SELECT	DepartmentID,
+		SUM(Salary) AS totalGaji 
+	FROM Employees
+	WHERE DepartmentID = @DepartmentID
+	GROUP BY DepartmentID
+END
+
+EXEC GetTotalSalaryByDepartmentMiardi
+@DepartmentID = 3;
 
 
 Soal 2: Membuat Stored Procedure untuk Update Data
@@ -10,6 +23,22 @@ Buat stored procedure UpdateEmployeeSalary untuk memperbarui gaji karyawan berda
 
 Ubah gaji karyawan dengan EmployeeID = 100 menjadi 20.000.000.
 
+CREATE PROCEDURE gantiGajiMiardi
+@EmployeeID INT,
+@Salary INT
+AS
+BEGIN
+	UPDATE Employees 
+	SET Salary = @Salary
+	WHERE EmployeeID = @EmployeeID
+END
+
+SELECT * FROM Employees
+WHERE EmployeeID = 13
+
+EXEC gantiGajiMiardi
+@EmployeeID = 13,
+@Salary = 1000000;
 
 
 Soal 3: Membuat Trigger untuk Audit
@@ -17,7 +46,25 @@ Buat trigger trg_AfterEmployeeUpdate untuk mencatat perubahan gaji ke tabel Empl
 
 Ubah gaji karyawan dengan EmployeeID = 200 menjadi 25.000.000 dan periksa tabel EmployeeAudit.
 
+CREATE TRIGGER trg_AfterEmployeeUpdateMiardi
+ON Employees
+AFTER OF UPDATE 
+AS
+BEGIN
+	INSERT INTO EmployeeAudit(EmployeeID, ChangeDate ,OldSalary, NewSalary)
+	SELECT inserted.EmployeeID, GETDATE(), deleted.Salary, inserted.Salary
+	FROM inserted
+	JOIN deleted
+	ON inserted.EmployeeID = deleted.EmployeeID	
+END
 
+DECLARE @salary INT = 25000000,
+	@employeeID INT = 55;
+UPDATE Employees 
+SET Salary = @salary
+WHERE EmployeeID = @employeeID
+
+SELECT * FROM EmployeeAudit ea 
 
 Soal 4: Membuat INSTEAD OF Trigger
 Buat trigger trg_CheckSalary untuk memastikan gaji tidak boleh lebih dari 100 juta.
