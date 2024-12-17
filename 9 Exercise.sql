e260ca4b2cf454b279ada6eb0caa3b763c5e0ab1
miardi <helmiyuliardi@gmail.com>
2024-12-17 12:11:59 +0700

    Latihan 9

--- 9 Exercise.sql
+++ 9 Exercise.sql
@@ -1,15 +1,28 @@
 Soal 1:
 Buat stored procedure bernama GetEmployeeDetails yang menerima parameter @DepartmentID dan @MinSalary. Stored procedure ini harus mengembalikan semua karyawan dari departemen tertentu yang gajinya lebih besar atau sama dengan @MinSalary.
 Jalankan stored procedure tersebut dengan parameter @DepartmentID = 5 dan @MinSalary = 5000
+CREATE PROCEDURE GetEmployeeDetailsMiardi
+@DepartmentID INT,
+@MinSalary INT
+AS
+BEGIN
+	SELECT * FROM Employees
+	WHERE DepartmentID = @DepartmentID
+	AND	Salary >= @MinSalary
+END
 
+EXEC GetEmployeeDetailsMiardi 
+@DepartmentID = 5, 
+@MinSalary = 5000;
 
 Soal 2:
 
 Berikan hak akses SELECT ke tabel Employees kepada pengguna User_A.
 Berikan hak akses EXECUTE untuk stored procedure GetEmployeeDetails kepada pengguna User_B.
 Cabut hak akses SELECT dari pengguna User_A.
-
-
+GRANT SELECT ON Employees TO User_A
+GRANT EXECUTE ON GetEmployeeDetailsMiardi TO User_B
+REVOKE SELECT ON Employees TO User_A
 
 Soal 3:
 
@@ -17,6 +30,14 @@ Buat clustered index pada kolom EmployeeID di tabel Employees.
 Buat non-clustered index pada kolom Salary.
 Tunjukkan bagaimana query berikut menggunakan indeks untuk mempercepat pencarian data berdasarkan gaji:
 SELECT EmployeeID, Name, Salary FROM Employees WHERE Salary > 8000;
+
+CREATE CLUSTERED INDEX 
+IX_EmployeeID ON
+Employees(EmployeeID)
+
+CREATE NONCLUSTERED INDEX
+IX_Salary ON
+Employees(Salary)
 
 
 
@@ -26,4 +47,22 @@ Buat stored procedure UpdateEmployeeSalary yang menerima @EmployeeID dan @NewSal
 Jalankan stored procedure tersebut untuk memperbarui gaji karyawan dengan EmployeeID = 100 menjadi 7500.
 Tambahkan non-clustered index pada kolom JobTitle dan optimalkan query berikut menggunakan indeks:
 SELECT EmployeeID, Name FROM Employees WHERE JobTitle = 'Manager';
+
+CREATE PROCEDURE UpdateEmployeeSalaryMiardi 
+ @EmployeeID INT,
+ @NewSalary INT
+ AS
+ BEGIN
+ 	UPDATE Employees 
+ 	SET Salary = @NewSalary
+ 	WHERE EmployeeID = @EmployeeID
+ END
+ 
+ CREATE NONCLUSTERED INDEX
+ IX_JobTitle ON
+ Employees(JobTitle)
+ 
+ EXEC UpdateEmployeeSalaryMiardi 
+ @EmployeeID = 105, @NewSalary = 7500;
+ 
 
